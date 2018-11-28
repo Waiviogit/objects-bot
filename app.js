ed4815acf124c1e04b97f0d96cbf8e1b9953fc96
Vladimir Klimenko <klimbox@gmail.com>
2018-11-28 13:50:56 +0200

    fix cors requests

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/', routes);
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+app.use('/', routes);
 
 module.exports = app;
