6bfead97272d7155a750ae77d9d6ba50da94b99c
Vladimir Klimenko <klimbox@gmail.com>
2019-02-13 14:53:25 +0200

    generate album id on front end side

--- helpers/dataMapper.js
+++ helpers/dataMapper.js
@@ -62,15 +62,12 @@ const getPostData = (type, reqData, accData) => {
             };
             break;
         case actionTypes.APPEND_OBJECT:
-            const galleryAlbumId = (reqData.field.name === 'galleryAlbum')
-                ? Math.random().toString(36).substring(2)
-                : null;
             appendObjPostData.parent_author = reqData.parentAuthor;
             appendObjPostData.parent_permlink = reqData.parentPermlink;
             metadata.wobj = {
                 action: type,
                 creator: reqData.author,
-                field: galleryAlbumId ? { ...reqData.field, id: galleryAlbumId } : { ...reqData.field },
+                field: reqData.field,
             };
             break;
         case actionTypes.FORECAST_EXPIRED:
