6fa67db385d7ca92f7e102df83ebb7c6ca3de1cf
Maxim Oancha <maksimoancha12@gmail.com>
2019-09-13 17:25:11 +0300

    change urlencoded

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use( cors() );
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', routes);
 
