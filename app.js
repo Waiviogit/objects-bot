f5c5659f910ea98052c9bbd49d3cbfa7c6729a56
Maxim Oancha <maksimoancha12@gmail.com>
2019-09-13 17:19:18 +0300

    change cors

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require( 'cors' );
 const { routes } = require('./routes');
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use( cors() );
 app.use(express.urlencoded({ extended: false }));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 app.use('/', routes);
 
 module.exports = app;
