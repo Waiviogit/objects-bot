c7453371cffd1cd3127f43b360ffb1bd08b7d81a
olegtsigulov <Oleg28101974>
2020-01-06 21:51:05 +0200

    add regExp

--- constants/regExp.js
+++ constants/regExp.js
@@ -1,4 +1,4 @@
-const steemErrRegExp = new RegExp('STEEM_MIN_ROOT_COMMENT_INTERVAL|RC. Please wait to transact, or power up STEEM');
+const steemErrRegExp = new RegExp('STEEM_MIN_ROOT_COMMENT_INTERVAL|RC. Please wait to transact, or power up STEEM|STEEM_MIN_REPLY_INTERVAL');
 
 
 module.exports = {
