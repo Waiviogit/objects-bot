524b964b3548015e1bb969fc538d22fa7a271237
olegtsigulov <olegcigulev@gmail.com>
2020-01-16 21:20:42 +0200

    refactor

--- constants/regExp.js
+++ constants/regExp.js
@@ -1,6 +1,6 @@
-const steemErrRegExp = new RegExp('STEEM_MIN_ROOT_COMMENT_INTERVAL|RC. Please wait to transact, or power up STEEM|STEEM_MIN_REPLY_INTERVAL');
+const steemErrRegExp = new RegExp( 'STEEM_MIN_ROOT_COMMENT_INTERVAL|RC. Please wait to transact, or power up STEEM|STEEM_MIN_REPLY_INTERVAL|trx.ref_block_prefix' );
 
 
 module.exports = {
-    steemErrRegExp,
+    steemErrRegExp
 };
