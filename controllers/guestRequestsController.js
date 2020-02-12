bfbbba984109c8cd75ea4b7ba08da594d6459a74
olegtsigulov <olegcigulev@gmail.com>
2020-02-12 16:27:11 +0200

    guest user can append objects

--- controllers/guestRequestsController.js
+++ controllers/guestRequestsController.js
@@ -4,11 +4,11 @@ const authoriseUser = require('utilities/authorazation/authoriseUser');
 const commentHelper = require('utilities/helpers/commentHelper');
 
 const proxyPosting = async (req, res, next) => { // add data to queue
- const comment = validationHelper.postingValidator(req.body, next);
+  const comment = validationHelper.postingValidator(req.body, next);
   if (!comment) return;
 
   const { error, isValid } = await authoriseUser.authorise(comment.comment.author);
- if (error) return next(error);
+  if (error) return next(error);
   if (isValid) {
     if (comment.comment.guest_root_author && comment.comment.parent_author !== '') {
       const result = await commentHelper.validateComment(comment.comment, next);
@@ -30,7 +30,7 @@ const proxyCustomJson = async (req, res, next) => {
   const result = await customJsonOperations.switcher(req.body, next);
 
   if (!result) return;
-  res.result = { status: 200 };
+  res.result = { status: 200, json: result };
   next();
 };
 
