c2a0a9880924b91aedc30562081053cdbd72845e
olegtsigulov <olegcigulev@gmail.com>
2020-03-21 16:06:19 +0200

    refactor. add hive

--- utilities/helpers/checkUsersForBlackList.js
+++ utilities/helpers/checkUsersForBlackList.js
@@ -3,7 +3,7 @@ const config = require('config');
 const apiRequests = require('utilities/waivioApi/apiRequests');
 
 const checkForBlackList = async (creator) => {
-  const { app, error } = apiRequests.getAppData({ name: config.app });
+  const { app, error } = await apiRequests.getAppData({ name: config.app });
   if (error) return true;
   return _.includes(app.black_list_users, creator);
 };
