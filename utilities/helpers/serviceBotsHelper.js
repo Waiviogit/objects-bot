b68af8fae6c9ada0817ce1cdb5e606c577506f8a
olegtsigulov <olegcigulev@gmail.com>
2020-03-24 12:06:56 +0200

    change logic for exp Forecast

--- utilities/helpers/serviceBotsHelper.js
+++ utilities/helpers/serviceBotsHelper.js
@@ -5,7 +5,7 @@ const apiRequests = require('utilities/waivioApi/apiRequests');
 
 exports.setEnvData = async () => {
   const { app, error } = await apiRequests.getAppData({ name: config.app });
-  if (!error) return { error };
+  if (error) return { error };
   if (!app) return { error: { message: 'App not found!' } };
   const serviceBots = _.filter(app.service_bots, (bot) => {
     if (_.includes(bot.roles, 'serviceBot')) return bot;
