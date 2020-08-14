7c72fd31f78c1ae73b8d9267871cd87627179e44
Maxim <maksimoancha12@gmail.com>
2020-08-14 11:28:14 +0300

    add block subscribe for guests

--- utilities/helpers/postingData.js
+++ utilities/helpers/postingData.js
@@ -32,8 +32,8 @@ const getOptions = async (reqData, accData, type) => {
   }
   optionsData.author = accData.name;
   optionsData.extensions = [[0, { beneficiaries }]];
-  optionsData.max_accepted_payout = '100000.000 HBD';
-  optionsData.percent_hive_dollars = 0;
+  optionsData.max_accepted_payout = '100000.000 SBD';
+  optionsData.percent_steem_dollars = 0;
   optionsData.allow_votes = true;
   optionsData.allow_curation_rewards = true;
   optionsData.permlink = reqData.permlink;
