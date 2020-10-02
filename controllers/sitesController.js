57a1c5fb58fc6e33d9d48b63f77c612b4f6fb0a0
olegtsigulov <olegcigulev@gmail.com>
2020-10-02 15:52:41 +0300

    add route for create site

--- controllers/sitesController.js
+++ controllers/sitesController.js
@@ -4,7 +4,7 @@ const sitesHelper = require('utilities/helpers/sitesHelper');
 exports.sendCreateSite = async (req, res, next) => {
   const value = validators.validate(req.body, validators.sites.createWebsite, next);
   if (!value) return;
-  if (req.headers.API_KEY !== process.env.API_KEY) return next({ status: 401, message: 'unauthorized' });
+  if (req.headers.api_key !== process.env.API_KEY) return next({ status: 401, message: 'unauthorized' });
   const { result, error } = await sitesHelper.sendCreateWebsite(value);
 
   if (error) return next(error);
