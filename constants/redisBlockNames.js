fc0547f89f5842054a06d4b84f346ebd45329d3b
fesmofet <borees2@gmail.com>
2022-08-23 19:29:16 +0300

    cache_service_bots

--- constants/redisBlockNames.js
+++ constants/redisBlockNames.js
@@ -1,2 +1,4 @@
 exports.LAST_BLOCK_NUM = 'last_block_num';
 exports.LAST_VOTE_BLOCK_NUM = 'last_vote_block_num';
+
+exports.CACHE_SERVICE_BOTS = 'service_bots';
--- utilities/helpers/serviceBotsHelper.js
+++ utilities/helpers/serviceBotsHelper.js
@@ -1,19 +1,33 @@
 const _ = require('lodash');
 const config = require('config');
 const apiRequests = require('utilities/waivioApi/apiRequests');
+const redisGetter = require('utilities/redis/redisGetter');
+const jsonHelper = require('utilities/helpers/jsonHelper');
+const { CACHE_SERVICE_BOTS } = require('constants/redisBlockNames');
 
 exports.setEnvData = async () => {
-  const { app, error } = await apiRequests.getAppData({ name: config.app });
+  const { bots, error } = await getCacheBots();
   if (error) return { error };
-  if (!app) return { error: { message: 'App not found!' } };
-  const serviceBots = _.filter(app.service_bots, (bot) => {
+  const serviceBots = _.filter(bots, (bot) => {
     if (_.includes(bot.roles, 'serviceBot')) return bot;
   });
-  const proxyBots = _.filter(app.service_bots, (bot) => {
+  const proxyBots = _.filter(bots, (bot) => {
     if (_.includes(bot.roles, 'proxyBot')) return bot;
   });
-  const reviewBots = _.filter(app.service_bots, (bot) => {
+  const reviewBots = _.filter(bots, (bot) => {
     if (_.includes(bot.roles, 'reviewBot')) return bot;
   });
   return { serviceBots, proxyBots, reviewBots };
+};
+
+const getCacheBots = async () => {
+  const bots = await redisGetter.smembersAsync({ key: CACHE_SERVICE_BOTS });
+  if (_.isEmpty(bots)) {
+    const { app, error } = await apiRequests.getAppData({ name: config.app });
+    if (error) return { error };
+    if (!app) return { error: { message: 'App not found!' } };
+    return { bots: app.service_bots };
+  }
+  const parsedBots = _.compact(_.map(bots, (el) => jsonHelper.parseJson(el, null)));
+  return { bots: parsedBots };
 };
