035605d15eda67562c4452aa40f3868b46367f6b
fesmofet <borees2@gmail.com>
2024-03-11 17:43:33 +0200

    add exposed fields to objectTypes

---
+++ utilities/tasks/addExposedFields.js
@@ -0,0 +1,25 @@
+const { EXPOSED_FIELDS_FOR_OBJECT_TYPE } = require('../../constants/wobjectsData');
+const { ObjectType } = require('../../database').models;
+
+const addExposedFields = async () => {
+  try {
+    const objectTypes = ObjectType.find({}).lean();
+
+    for await (const type of objectTypes) {
+      const exposedFields = EXPOSED_FIELDS_FOR_OBJECT_TYPE[type?.name];
+      if (!exposedFields?.length) continue;
+      await ObjectType.updateOne(
+        { _id: type._id },
+        { $addToSet: { exposedFields: { $each: exposedFields } } },
+      );
+    }
+    console.log('task finished');
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+(async () => {
+  await addExposedFields();
+  process.exit();
+})();
