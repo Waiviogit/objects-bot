e0b2cbab9350b9ee4c5e8e849e24e7f8fcbaca58
fesmofet <borees2@gmail.com>
2024-03-22 11:12:53 +0200

    fix blacklist

--- utilities/waivioApi/apiRequests.js
+++ utilities/waivioApi/apiRequests.js
@@ -4,7 +4,7 @@ const _ = require('lodash');
 
 exports.getAppData = async ({ name }) => {
   try {
-    const result = await axios.get(`https://${config.waivio_auth.host}/api/app/${name}`, { headers: { 'api-key': process.env.API_KEY } });
+    const result = await axios.get(`https://${config.waivio_auth.host}/api/app/${name}`);
     return { app: result.data };
   } catch (error) {
     return { error };
