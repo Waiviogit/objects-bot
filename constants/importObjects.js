582ab2c08191841a010e6e04b4d2eca4da6989e7
fesmofet <borees2@gmail.com>
2024-05-01 17:07:13 +0300

    vote ability decrease

--- constants/importObjects.js
+++ constants/importObjects.js
@@ -1,9 +1,9 @@
 exports.WHITE_LIST_KEY = 'white_list_import';
 
 exports.VOTE_COST = {
-  INITIAL: 0.01,
-  USUAL: 0.001,
-  FOR_WHITE_LIST: 0.0001,
+  INITIAL: 0.001,
+  USUAL: 0.0001,
+  FOR_WHITE_LIST: 0.00001,
 };
 
 exports.IMPORT_REDIS_KEYS = {
